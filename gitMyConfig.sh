c53c01a863a7be1103493468d3a45fe8d5eddcf5
Tyler Niles <tniles09@gmail.com>
2016-03-01 14:46:51 -0800

    add push simple spec for cygwin git

--- gitMyConfig.sh
+++ gitMyConfig.sh
@@ -7,6 +7,8 @@
 git config --global user.name "Tyler Niles"
 # read line: grab email from user input
 #git config --global user.email " "
+# git2.0 specification (for cygwin warnings)
+git config --global push.default simple
 
 # core
 
