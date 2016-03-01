8ee0870d20f6b80e9321b3f05c1915db22fdf607
Tyler Niles <tniles09@gmail.com>
2016-03-01 15:52:33 -0800

    difftool notes for cygwin

--- gitMyConfig.sh
+++ gitMyConfig.sh
@@ -13,3 +13,7 @@ git config --global push.default simple
 # core
 
 # diff
+# NOTE: on cygwin, you may have something like kdiff3-qt installed, and then git won't be able
+# to find it since it is expecting 'kdiff3'. My workaround is to add a symbolic link next to
+# the kdiff3-qt, e.g. ln -s kdiff3-qt kdiff3. 
+git config --global diff.tool kdiff3
