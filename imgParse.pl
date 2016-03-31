d6a7b25dd45069fe146f6986ed78fbb0d781db44
Tyler Niles <tniles09@gmail.com>
2016-03-31 16:38:21 -0700

    Image parser to break into even blocks of transferrable data and pad last block with zeroes.

--- imgParse.pl
+++ imgParse.pl
@@ -25,29 +25,31 @@ use 5.010;
 use warnings;
 #use strict;
 
+use POSIX qw(ceil floor);
+
+# See perldoc.perlport for more on cross-platform line ending handling.
+#$/ = "\r\n";        # chomp: account for windows eol
+
 
 ###########
 # vars    #
 ###########
  
 my $blockSize = 32;
-my $numBlocks = 4;
 
-my $prefix = S31;
-#my sysCmd = "0x1001"
-#my blockNum = "0x0000"
-#my dataChunk = ""
+my $inPrefix = "S31";
+
+# FORM: $outPrefix . $outCommand . $outBlockNum . $outPayload . $outSuffix
+my $outPrefix   = "CMD=";
+my $outSuffix   = "==";
+my $outCommand  = "0x1001";
+my $outBlockNum = "0x0000";
 
 
 ###########
 # program #
 ###########
 
-# Opting for the diamond operator, see below
-#if ( ! open IMAGE, "<", "new.bin" )
-#{
-  #die "$0: Cannot open input file. $!"
-#}
 if ( @ARGV < 1 )
 {
   die "Must supply input image file; exiting $!"
@@ -58,23 +60,43 @@ if ( ! open CMDS, ">", "dbgDownload.txt" )
   die "$0: Cannot open output file. $!"
 }
 
-print ("Program:\t$0\nInput:\t\t@ARGV\n\n");
+#print ("Program:\t$0\nInput:\t\t@ARGV\n\n");
 
 # read lines from image file in
 say "Processing File...";
+
 #while (defined($line = <>))
 #{
   #chomp($line);
   #say CMDS "Here's your line: $line";
 #}
 # Equivalently:
+
 while (<>)
 {
-  next if $. == 1;        # skip first line (header of srec)
+  next if $. == 1;        # srec: skip first line (header of srec)
   chomp;
-  s/^$prefix//g;          # remove srec prefix
-  s/(...)$//g;            # remove two char checksum suffix
-  say CMDS "$_";          # using default perl var
+  s/^$inPrefix//g;        # srec: remove prefix
+  s/(...)$//g;            # srec: remove two char checksum and carriage return suffix
+  $data .= $_;            # grab all image data
+}
+
+say "     There are " . (length $data) .
+    " number of bytes and " . ceil((length $data) / $blockSize) .
+    " blocks to be transferred.";
+
+for ( $ijk = 0; $ijk < (length $data); $ijk += $blockSize )
+{
+  $outPayload = substr ( $data, $ijk, $blockSize );
+  if ( length $outPayload < 32 )
+  {
+    # pad with zeroes
+    $outPayload .= (0 x 32);
+    $outPayload = substr ( $outPayload, 0, $blockSize );
+  }
+  say CMDS $outPayload;      # good for debugging correct block size, padding, etc.
+  #say CMDS $outPrefix . $outCommand . $outBlockNum . $outPayload . $outSuffix;
+
 }
 
 say "All Done!";
@@ -84,6 +106,5 @@ say "All Done!";
 # cleanup #
 ###########
  
-#close IMAGE;
 close CMDS;
 exit 0
