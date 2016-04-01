4e500df5f0d1c0d21dbf819298bda23068687518
Tyler Niles <tniles09@gmail.com>
2016-04-01 15:12:41 -0700

    adding hex byte swap script

---
+++ hexSwap.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl --
+#
+# Script to print byte-swapped hex values
+#
+
+use 5.010;
+use warnings;
+use strict;
+
+my $xData;
+my $yData;
+my $zData;
+
+for ( my $ijk = 998; $ijk < 1001; $ijk++ )
+{
+  printf ( "\n%4d is hex " . (sprintf "0x%04X", $ijk) . "\n", $ijk );
+
+  # with byte swap
+  say "These numbers (bytes swapped) should match...";
+
+  # do sprintf, match pattern and store to ($1)($2), now reverse them into ($2)($1). 
+  # BindOp leaves $_ alone, match stuffs $_ & is then used as input for reverse, prints.
+  say reverse ((sprintf "%04X", $ijk) =~ /(..)(..)/) ;        # from perlmonks' webpage
+
+  $yData = (reverse ((sprintf "%04X", $ijk) =~ /(..)(..)/) );
+  say $yData;                         # does NOT match
+
+  $xData = sprintf "%04X", $ijk;
+  $xData =~ s/(..)(..)/$2$1/ ;
+  say $xData;                         # does match
+
+  $_ = sprintf "%04X", $ijk;
+  /(..)(..)/;
+  $zData = $2 . $1 ;
+  say $zData;                         # does match
+}
+
+exit 0;
